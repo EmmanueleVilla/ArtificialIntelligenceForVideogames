ef71a096a556dc1ee24876efb6f22a7548c06bec
EmmanueleVilla <emmanuele.villa@finconsgroup.com>
2021-11-24 11:04:44 +0100

    fix initiative update

--- Assets/Scripts/Game/Managers/InitiativeUIManager.cs
+++ Assets/Scripts/Game/Managers/InitiativeUIManager.cs
@@ -12,27 +12,37 @@ public class InitiativeUIManager : MonoBehaviour
     public GameManager GameManager;
     public Text InitiativeText;
 
+    List<ICreature> _initiatives;
+
     void Start()
     {
         GameManager.InitiativesRolled += GameManager_InitiativesRolled;
     }
 
+    void Update()
+    {
+        if(_initiatives != null)
+        {
+            var battle = DndModule.Get<IDndBattle>();
+            var creatureInTurn = battle.GetCreatureInTurn();
+
+            var builder = new StringBuilder();
+            foreach (var creature in _initiatives)
+            {
+                if (creatureInTurn == creature)
+                {
+                    builder.Append("> ");
+                }
+                builder.AppendLine(creature.GetType().ToString().Split('.').Last());
+            }
+
+            InitiativeText.text = builder.ToString();
+        }
+    }
+
     private void GameManager_InitiativesRolled(object sender, List<ICreature> initiatives)
     {
-        var battle = DndModule.Get<IDndBattle>();
-        var creatureInTurn = battle.GetCreatureInTurn();
-
-        var builder = new StringBuilder();
-        foreach (var creature in initiatives)
-        {
-            if (creatureInTurn == creature)
-            {
-                builder.Append("> ");
-            }
-            builder.AppendLine(creature.GetType().ToString().Split('.').Last());
-        }
-
-        InitiativeText.text = builder.ToString();
+        _initiatives = initiatives;
     }
 
 }
